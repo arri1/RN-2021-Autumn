7b6240ae7b8cb303023a2060be4f97de3e10c60a
Deslifer <kolmingstar@mail.ru>
2021-09-14 23:33:51 +0900

    Image test

--- pirozhkovAleksandr/App.js
+++ pirozhkovAleksandr/App.js
@@ -1,18 +1,53 @@
 import React from 'react';
 import type {Node} from 'react';
+import Background from './back.jpg';
+import Saitama from './saitama.png';
+import SaitamaOk from './saitamaOk.png';
+import Punch from './punch.gif';
+
 
 import{
+  View,
 	SafeAreaView,
-	Text
+	Text,
+  Image,
+  StyleSheet,
+  ImageBackground, 
 }from 'react-native';
 
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 18,
+    marginTop: -165,
+    marginLeft: 275,
+  },
+  image: {
+    marginTop: '70%',
+    marginLeft: '10%',
+    alignSelf: 'center',
+  },
+  back:{
+    height:'100%',
+  }  
+});
 
 const App: () => Node = () => {
-
   return (
-    <SafeAreaView>
-      <Text>Hi</Text>
-    </SafeAreaView>
+ 
+    <View>
+      
+      <ImageBackground source={Background} style={styles.back}>
+      <Image 
+      style={styles.image} 
+      source={SaitamaOk}
+      height={200}
+      width={240}
+       />
+      <Text style={styles.text}>Hello{"\n"}world!</Text>
+      </ImageBackground>
+    </View>
+    
+    
   );
 };
 
