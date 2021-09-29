b480222c6178d03d985241c7d538310ce52fd6da
Deslifer <kolmingstar@mail.ru>
2021-09-29 19:55:59 +0900

    lab1

--- pirozhkovAleksandr/App.js
+++ pirozhkovAleksandr/App.js
@@ -1,9 +1,6 @@
-import React from 'react';
-import type {Node} from 'react';
-import Background from './back.jpg';
-import Saitama from './saitama.png';
-import SaitamaOk from './saitamaOk.png';
-import Punch from './punch.gif';
+import React, {useState} from 'react';
+
+import toasty from './toasty.png';
 
 
 import{
@@ -13,39 +10,101 @@ import{
   Image,
   StyleSheet,
   ImageBackground, 
+  TouchableOpacity,
+  ScrollView
 }from 'react-native';
 
 const styles = StyleSheet.create({
-  text: {
-    fontSize: 18,
-    marginTop: -165,
-    marginLeft: 275,
+  main:{
+    height: '100%',
   },
-  image: {
-    marginTop: '70%',
-    marginLeft: '10%',
+  box:{
+    height: 150,
+    width: 200,
     alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    borderRadius: 20,
   },
-  back:{
-    height:'100%',
-  }  
+  littleBox:{
+    height: 50,
+    width: 200,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+    borderRadius: 15,
+    backgroundColor: '#DBAB84'
+  },
+  text:{
+    fontSize: 20,
+
+  },
+  counterContainer:{
+    width: 200,
+    borderRadius: 10,
+    alignItems: 'center',
+    backgroundColor: '#DBBD86',
+    marginTop: 20,
+    alignSelf: 'center',
+    fontSize: 20,
+  },
+  image:{
+    position: 'absolute',
+    top: 60,
+    bottom: 0,
+    left: 150,
+    right: 0,
+  }
 });
 
-const App: () => Node = () => {
+const App = () =>  {
+  const [color, setColor] = useState('#4A398F');
+  const [counter, setCounter] = useState(0);
+  const [shouldShow, setShouldShow] = useState(false);
+
   return (
- 
-    <View>
-      
-      <ImageBackground source={Background} style={styles.back}>
-      <Image 
-      style={styles.image} 
-      source={SaitamaOk}
-      height={200}
-      width={240}
-       />
-      <Text style={styles.text}>Hello{"\n"}world!</Text>
-      </ImageBackground>
-    </View>
+    <SafeAreaView style={[styles.main, {backgroundColor: color}]}>
+      <ScrollView>
+        <View style={styles.image}>
+          {shouldShow ? (
+       
+            <Image
+              source={toasty}
+            />
+          ): null}
+        </View>
+
+        <TouchableOpacity
+          style = {[styles.box,{backgroundColor: '#58DB5A'},{marginTop: 150}]}
+          onPress={()=>setColor('#458F46')}>
+          <Text style={styles.text}>Click me</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style = {[styles.box,{backgroundColor: '#836EDB'}]}
+          onPress={()=>setColor('#4A398F')}>
+          <Text style={styles.text}>Click me too</Text>
+        </TouchableOpacity>
+        
+        <View style={styles.counterContainer}>
+          <Text style={styles.text}>{counter}</Text>
+        </View>
+
+        <TouchableOpacity
+          style = {styles.littleBox}
+          onPress={()=> setCounter(counter + 1)}>
+          <Text style={styles.text}>Add some digits</Text>
+        </TouchableOpacity>
+
+        <TouchableOpacity
+          style = {[styles.littleBox,{backgroundColor: '#B16FDB'}]}
+          onPress={()=>setShouldShow(!shouldShow)}>
+          <Text style={styles.text}>Toasty</Text>
+        </TouchableOpacity>
+
+      </ScrollView>
+    </SafeAreaView>
     
     
   );
