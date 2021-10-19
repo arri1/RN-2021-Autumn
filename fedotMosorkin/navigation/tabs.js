61877fa1863c97d6490254d3084af1527257e224
Silverthg <fmosorkin@gmail.com>
2021-10-20 03:26:50 +0900

    lab1

---
+++ fedotMosorkin/navigation/tabs.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Lab1 from '../components/screens/Lab1';
+import Lab2 from '../components/screens/Lab2';
+
+const Tab = createBottomTabNavigator();
+
+const Tabs = () => {
+    return(
+        <Tab.Navigator>
+            <Tab.Screen name="Lab1" component={Lab1}/>
+            <Tab.Screen name="Lab2" component={Lab2}/>
+        </Tab.Navigator>
+    );
+};
+
+export default Tabs;
