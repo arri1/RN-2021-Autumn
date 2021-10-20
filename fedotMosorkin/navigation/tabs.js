f4cb24d10b16b29ebd2452e2a44bc44506abe591
Silverthg <fmosorkin@gmail.com>
2021-10-20 18:39:23 +0900

    lab1fixed_1

--- fedotMosorkin/navigation/tabs.js
+++ fedotMosorkin/navigation/tabs.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Lab1 from '../components/screens/Lab1';
-import Lab2 from '../components/screens/Lab2';
 
 const Tab = createBottomTabNavigator();
 
@@ -10,7 +9,7 @@ const Tabs = () => {
     return(
         <Tab.Navigator>
             <Tab.Screen name="Lab1" component={Lab1}/>
-            <Tab.Screen name="Lab2" component={Lab2}/>
+
         </Tab.Navigator>
     );
 };
