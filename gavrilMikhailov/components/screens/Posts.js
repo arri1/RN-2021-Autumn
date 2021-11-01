e4343ca89f5ad1c1226b2a2578ce99e50ea60453
Михайлов Гаврил Егорович <mihailov_ge@albank.ru>
2021-11-01 20:19:02 +0900

    Revert "Удаление второй лабы"
    This reverts commit b5f3a749ce625877d6700e102360f6a58269acc4.

---
+++ gavrilMikhailov/components/screens/Posts.js
@@ -0,0 +1,14 @@
+import React from "react"
+import { 
+  View, 
+  SafeAreaView
+} from "react-native"
+
+const Posts = () => {
+  return (
+    <SafeAreaView>
+      <View></View>
+    </SafeAreaView>
+  )
+}
+export default Posts
