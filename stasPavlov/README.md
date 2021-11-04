2ede9a139f7c57468649c0b650ef989e8ca86ccb
y2kk <scotlandfjord@protonmail.com>
2021-11-04 18:11:17 +0900

    [ref] inline styles to classes

--- stasPavlov/README.md
+++ stasPavlov/README.md
@@ -1,2 +1,8 @@
-Commit messages style: https://github.com/zevlg
+Commit messages style:
+    [enh]: enhancement
+    [add]: add
+    [fix]: fix
+    [ref]: refactoring
+    [rem]: remove
+
 Figma link: https://www.figma.com/file/VD5RPo1bZWA2hbu5vXbY9B/Figma-MobDev?node-id=0%3A1
--- stasPavlov/src/screens/Lab2.tsx
+++ stasPavlov/src/screens/Lab2.tsx
@@ -23,6 +23,17 @@ const styles = StyleSheet.create({
     todoText: {
         fontSize: 15,
     },
+    header: {
+        width: '90%',
+        marginTop: '5%',
+    },
+    headerInfo: {
+        flexDirection: 'row',
+        width: '90%',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 10,
+    },
     heading: {
         fontSize: 20,
         width: '50%',
@@ -32,15 +43,87 @@ const styles = StyleSheet.create({
         padding: 2,
         backgroundColor: '#AAA',
     },
+    date: {
+        backgroundColor: colors.gray,
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 50,
+    },
     todo: {
         width: dimensions.width * 0.40,
-        height: dimensions.height * 0.3,
+        height: dimensions.height * 0.25,
         marginHorizontal: dimensions.width * 0.02,
         marginVertical: dimensions.width * 0.02,
         borderRadius: 40,
         borderWidth: 3,
         borderColor: colors.gray,
         padding: 20,
+    },
+    todoList: {
+        width: '90%',
+        marginBottom: '10%',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'flex-start',
+    },
+    todoListHeading: {
+        marginRight: '20%',
+        borderBottomWidth: 5,
+        borderBottomColor: colors.orange,
+    },
+    todoListHeader: {
+        width: '90%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        marginVertical: 20,
+    },
+    taskCreatorHeadingContainer: {
+        width: '100%',
+        marginTop: 20,
+        marginBottom: 10,
+    },
+    taskCreatorHeading: {
+        fontSize: 13,
+        marginLeft: '10%',
+    },
+    taskCreator: {
+        width: '90%',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+    },
+    taskCreatorLeft: {
+        justifyContent: 'center',
+        width: '68%'
+    },
+    submitButton: {
+        width: '28%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 20,
+    },
+    headingInput: {
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        margin: 10,
+        padding: 10,
+        height: 30,
+        fontSize: 10,
+    },
+    textInput: {
+        backgroundColor: colors.white,
+        borderRadius: 10,
+        padding: 10,
+        marginHorizontal: 10,
+        marginBottom: 10,
+        fontSize: 10,
+    },
+    submitButtonText: {
+        fontSize: 13,
+        color: colors.white,
+    },
+    tasksCounter: {
+        fontSize: 18,
+        color: colors.lightLilac,
     },
 })
 
@@ -72,15 +155,9 @@ const Lab2: React.FC = () => {
         <SafeAreaView style={styles.container}>
             <ScrollView showsVerticalScrollIndicator={false}>
                 <View style={{alignItems: 'center', justifyContent: 'center'}}>
-                    <View style={{width: '90%', marginTop: '5%'}}>
+                    <View style={styles.header}>
                         <MediumText style={styles.heading}>Good morning, whoever you are</MediumText>
-                        <View style={{
-                            flexDirection: 'row',
-                            width: '90%',
-                            justifyContent: 'space-between',
-                            alignItems: 'center',
-                            marginTop: 10,
-                        }}>
+                        <View style={styles.headerInfo}>
                             <View style={{
                                 flexDirection: 'row',
                                 alignItems: 'center',
@@ -88,12 +165,7 @@ const Lab2: React.FC = () => {
                                 <View style={{marginRight: 10}}>
                                     <CalendarIcon />
                                 </View>
-                                <View style={{
-                                    backgroundColor: colors.gray,
-                                    paddingVertical: 5,
-                                    paddingHorizontal: 15,
-                                    borderRadius: 50,
-                                }}>
+                                <View style={styles.date}>
                                     <SemiBoldText style={{fontSize: 13}}>
                                         {moment().format('Do MMM')}
                                     </SemiBoldText>
@@ -102,83 +174,41 @@ const Lab2: React.FC = () => {
                             <MediumText>{todos.length} tasks to do today</MediumText>
                         </View>
                     </View>
-                    <View
-                        style={{
-                            width: '100%',
-                            marginTop: 20,
-                            marginBottom: 10,
-                        }}
-                    >
-                        <SemiBoldText style={{
-                            fontSize: 13,
-                            marginLeft: '8%',
-                        }}>What's your task for today?</SemiBoldText>
+                    <View style={styles.taskCreatorHeadingContainer}>
+                        <SemiBoldText style={styles.taskCreatorHeading}>What's your task for today?</SemiBoldText>
                     </View>
                     <View
-                        style={{
-                            width: '90%',
-                            flexDirection: 'row',
-                            justifyContent: 'space-between',
-                        }}
+                        style={styles.taskCreator}
                     >
-                        <View style={{justifyContent: 'center', width: '68%'}}>
+                        <View style={styles.taskCreatorLeft}>
                             <View style={{backgroundColor: colors.gray, borderRadius: 20}}>
                                 <TextInput
                                     placeholder={'Your task\'s heading'}
-                                    style={{
-                                        backgroundColor: colors.white,
-                                        borderRadius: 10,
-                                        margin: 10,
-                                        padding: 10,
-                                        height: 30,
-                                        fontSize: 10,
-                                    }}
+                                    style={styles.headingInput}
                                     value={newTaskHeading}
                                     onChangeText={setNewTaskHeading}/>
                                 <TextInput
                                     placeholder={'Write your task here...'}
                                     multiline
-                                    style={{
-                                        backgroundColor: colors.white,
-                                        borderRadius: 10,
-                                        padding: 10,
-                                        marginHorizontal: 10,
-                                        marginBottom: 10,
-                                        fontSize: 10,
-                                    }}
+                                    style={styles.textInput}
                                     value={newTaskText}
                                     onChangeText={setNewTaskText}/>
                             </View>
                         </View>
-                        <TouchableOpacity style={{
-                            width: '28%',
-                            justifyContent: 'center',
-                            alignItems: 'center',
-                            backgroundColor: newTaskText && newTaskHeading ? colors.purple : colors.gray,
-                            borderRadius: 20,
-                        }}
-                        disabled={!(newTaskText && newTaskHeading)}
-                        onPress={submitTask}
+                        <TouchableOpacity style={[styles.submitButton, {backgroundColor: newTaskText && newTaskHeading ? colors.purple : colors.gray}]}
+                            disabled={!(newTaskText && newTaskHeading)}
+                            onPress={submitTask}
                         >
-                            <BoldText style={{fontSize: 13, color: colors.white}}>Submit</BoldText>
+                            <BoldText style={styles.submitButtonText}>Submit</BoldText>
                         </TouchableOpacity>
                     </View>
-                    <View style={{
-                        width: '90%',
-                        flexDirection: 'row',
-                        alignItems: 'center',
-                        marginVertical: 20,
-                    }}>
-                        <View style={{
-                            marginRight: '20%',
-                            borderBottomWidth: 5,
-                            borderBottomColor: colors.orange,
-                        }}>
+                    <View style={styles.todoListHeader}>
+                        <View style={styles.todoListHeading}>
                             <SemiBoldText style={{fontSize: 22 }}>Tasks</SemiBoldText>
                         </View>
-                        <SemiBoldText style={{fontSize: 18, color: colors.lightLilac}}>{todos.length} tasks</SemiBoldText>
+                        <SemiBoldText style={styles.tasksCounter}>{todos.length} tasks</SemiBoldText>
                     </View>
-                    <View style={{width: '90%', marginBottom: '10%', flexDirection: 'row', flexWrap: 'wrap', justifyContent: 'flex-start'  }}>
+                    <View style={styles.todoList}>
                         {todos && todos.map((todo : Todo) => {
                             return (
                                 <View key={v4()} style={styles.todo}>
