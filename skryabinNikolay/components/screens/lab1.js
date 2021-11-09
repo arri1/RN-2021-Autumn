fedda04aa6e5dbb3773c0204738a79c5caef8a61
skryabinNikolay <saykolyan1996@gmail.com>
2021-11-09 22:31:44 +0900

    Upgrade to Lab2 and fix const name

--- skryabinNikolay/components/screens/lab1.js
+++ skryabinNikolay/components/screens/lab1.js
@@ -8,7 +8,7 @@ import {
   Text,
 } from 'react-native';
 
-const lab1 = () => {
+const Lab1 = () => {
   const [color1, setColor1] = useState('#98DB46');
   const [color2, setColor2] = useState('#FFA500');
   const [color3, setColor3] = useState('#9E2D1E');
@@ -78,4 +78,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default lab1;
+export default Lab1;
