0735d13f837f01569ed88d83a00cd2fee9ccc656
unknown <spacersemenov2000@gmail.com>
2021-11-09 21:10:20 +0900

    lab1 fix

--- semenovArtyom/App.js
+++ semenovArtyom/App.js
@@ -8,8 +8,8 @@ import {
   Text,
 } from 'react-native';
 
-let randomHex = () => {
-  let letters = "0123456789ABCDEF";
+const randomHex = () => {
+  const letters = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
       color += letters[Math.floor(Math.random() * 16)];
