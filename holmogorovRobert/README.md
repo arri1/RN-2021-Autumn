6c912b1167de73419dac68c3f69487f03817685b
Kim1350 <70932884+Kim1350@users.noreply.github.com>
2021-11-10 15:30:21 +0900

    Добавил ссылку на фигма

---
+++ holmogorovRobert/README.md
@@ -0,0 +1,1 @@
+<https://www.figma.com/file/z9BCOWFH8nZgjA1IJOG2w4/FigmaLab2_5?node-id=0%3A1>
--- holmogorovRobert/components/main.js
+++ holmogorovRobert/components/main.js
@@ -1,6 +1,5 @@
 import React from "react";
-
-import { StyleSheet, Button, View, Text} from "react-native";
+import { StyleSheet, View, Text} from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 const Stack = createNativeStackNavigator();
