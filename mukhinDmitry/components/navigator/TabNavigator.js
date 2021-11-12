00d7b27f82f980b846dbe4e9642015e596c24d36
Dmitry Mukhin <dmitry.mukhin@mytona.com>
2021-11-12 12:23:18 +0900

    added bottom navbar tab icons

--- mukhinDmitry/components/navigator/TabNavigator.js
+++ mukhinDmitry/components/navigator/TabNavigator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Image, StyleSheet, Text  } from 'react-native';
 import rnLab1 from '../labs/lab1/Lab1';
 import rnLab2 from '../labs/lab2/Lab2';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -7,20 +8,40 @@ const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
     return (
-        <Tab.Navigator 
-            backgroundColor='#5E5E5E'
+        <Tab.Navigator screenOptions = {({ route }) => ({
+            headerStyle: {
+                backgroundColor: '#5E5E5E',
+            },
+            headerTitleAlign: 'center',
+            tabBarLabel: ({ focused }) => {
+                return <Text style={{
+                  color: focused ? "#FFFFFF" : "#A5A5A5",
+                  top: -8
+                }}>
+                  {route.name}
+                </Text>
+            },
+            tabBarStyle: {
+                elevation: 0,
+                backgroundColor: '#5E5E5E',
+                borderTopColor: '#5E5E5E',
+                borderTopWidth: 1,
+                height: 69.81,
+            }
+          })}
         >
             <Tab.Screen 
                 name="Lab 1" 
                 component={rnLab1} 
+                
                 options={{
                     title: 'Color lightener',
-                    headerStyle: {
-                        backgroundColor: '#5E5E5E',
-                    },
-                    headerTintColor: '#fff',
                     headerTitleStyle: {
                         fontWeight: 'bold',
+                        color: '#FFFFFF'
+                    },
+                    tabBarIcon: () => {
+                        return <Image source = {require("../../res/img/navbar/tab-1.png")} style={styles.icon} />;
                     },
                 }}
             />
@@ -28,18 +49,26 @@ const TabNavigator = () => {
                 name="Lab 2"
                 component={rnLab2}
                 options={{
-                    title: 'Color lightener',
-                    headerStyle: {
-                        backgroundColor: '#5E5E5E',
-                    },
-                    headerTintColor: '#fff',
+                    title: 'RN To-Do List',
                     headerTitleStyle: {
                         fontWeight: 'bold',
+                        color: '#FFFFFF'
+                    },
+                    tabBarIcon: () => {
+                        return <Image source = {require("../../res/img/navbar/tab-2.png")} style={styles.icon} />;
                     },
                 }}
                 />
         </Tab.Navigator>
     )
 }
+
+const styles = StyleSheet.create({
+    icon: {
+      width: 32,
+      height: 32,
+      resizeMode: 'stretch',
+    }
+  });
 
 export default TabNavigator
