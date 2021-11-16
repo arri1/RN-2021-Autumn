9dfcb36380671bfefcc213a7ad9b0e2a781ac7ee
Ambros52 <vladapug52@gmail.com>
2021-11-16 12:55:35 +0900

    Добавлена 3 лаба

---
+++ ambrosevaVlada/components/screens/Lab3.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { 
+  View, 
+  StyleSheet,
+} from 'react-native';
+
+import SlowObject from '../SlowObject';
+import QuickObject from '../QuickObject';
+
+
+const Lab3 = () => {
+
+  return (
+    <View style={styles.container}>
+      <SlowObject/>
+      <QuickObject/>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#FFFFFC',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+});
+
+export default Lab3;
