3ca57bea79ff257b23e5303ed3e0430c557d9a33
Valery Glushkov <valery.Glushkov@mytona.com>
2021-11-17 18:08:29 +0900

    add redux and redux example

--- glushkovValeryi/components/routers/TabNavigator.js
+++ glushkovValeryi/components/routers/TabNavigator.js
@@ -4,6 +4,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import Lab1 from '../screens/Lab1'
 import Lab2 from '../screens/Lab2'
+import Lab3 from '../screens/Lab3'
 import Lab4 from '../screens/Lab4'
 
 const Tab = createBottomTabNavigator();
@@ -42,6 +43,7 @@ const TabNavigator = () => {
         >
             <Tab.Screen name = "lab1" component = {Lab1} />
             <Tab.Screen name = "lab2" component = {Lab2} />
+            <Tab.Screen name = "lab3" component = {Lab3} />
             <Tab.Screen name = "lab4" component = {Lab4} />
         </Tab.Navigator>
     );
