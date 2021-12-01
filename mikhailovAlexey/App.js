d74d31aaa462807e4a1f3d942c8e42a8a2b74e66
lex1olnk <lex1olnk@gmail.com>
2021-12-01 18:34:31 +0900

    lab 1 fixed

--- mikhailovAlexey/App.js
+++ mikhailovAlexey/App.js
@@ -1,67 +1,15 @@
 import * as React from 'react';
-import {useState} from 'react';
-import { Text, View, StyleSheet, Button, Animated } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import styles from './components/styles/styles'
+
+import MyTabs from './components/routers/MyTabs';
+
 const App = () => {
-  const [animation, setAnimation] = React.useState(new Animated.Value(0))
-  const boxInterpolation =  animation.interpolate({
-    inputRange: [0, 100],
-    outputRange:['orange' , 'blue']
-  })
-  const animatedStyle = {
-    backgroundColor: boxInterpolation
-  }
-  const handleAnimation = () => {
-    if (animation.__getValue() == 100)
-      {
-        Animated.timing(animation, {
-           toValue: 0,
-           duration: 500,
-           useNativeDriver: false
-        }).start()
-      }
-      else
-      {
-        Animated.timing(animation, {
-          toValue: 100,
-          duration: 500,
-          useNativeDriver: false
-        }).start()
-      }
-    }
   return (
-    <Animated.View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' , backgroundColor: boxInterpolation }} >
-         <View style={styles.buttonRow}>
-               <Button style={styles.buttonSize} title="Update" onPress={handleAnimation}/>
-         </View>
-    </Animated.View>
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#444"
-  },
-  buttonRow: {
-    flexBasis: 100,
-    justifyContent: "space-evenly",
-    marginVertical: 20
-  },
-  buttonSize: {
-    fontSize: 30,
-    marginVertical: 30,
-    padding: 30
-  },
-  blockSize: {
-    backgroundColor: 'gray',
-    width: 200,
-    margin: 5,
-    height: 30,
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
-
 export default App;
+
---
+++ mikhailovAlexey/components/styles/styles.js
@@ -0,0 +1,110 @@
+import {StyleSheet} from 'react-native';
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+    backgroundColor: '#FFD232',
+    height: 1120
+  },
+  containerBackgroundColor: {
+    backgroundColor: '#FFD232',
+  },
+  box: {
+    backgroundColor: '#FF8B03',
+    borderRadius: 5,
+    justifyContent: 'center',
+    alignItems: "center",
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 20
+  },
+  boxSize: {
+    backgroundColor: '#FF8B03',
+    width: 380,
+    margin: 5,
+    height: 100,
+    justifyContent: 'center',
+    color: 'white',
+    elevation: 4,
+  },
+  boxTextStyle: {
+    color: "white",
+    marginLeft: 10,
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular'
+  },
+  navBarBox: {
+    top: 0,
+    height: 75,
+    backgroundColor: '#454545',
+    color: 'white',
+  },
+  navBarIcon : {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#FF8B03",
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  navBarIconText : {
+    color: 'white',
+    fontSize: 14,
+    fontFamily: 'Montserrat-Regular',
+  },
+  button: {
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: '#FF8B03',
+    elevation: 4,
+  },
+  buttonText: {
+    fontFamily: 'Montserrat-Regular',
+    fontSize: 20,
+    color: 'white',
+  },
+  memoButton: {
+    zIndex: -1,
+    position: 'absolute',
+    justifyContent: "center",
+    alignItems: "center",
+    width: 100,
+    height: 40,
+    borderRadius: 5,
+    backgroundColor: '#FF8B03',
+    elevation: 4,
+    top: 195,
+  },
+  memoButtonLeft: {
+    left: 15,
+  },
+  memoButtonRight: {
+    right: 15,
+  },
+  memoBoxSize: {
+    backgroundColor: '#FF8B03',
+    width: 380,
+    margin: 5,
+    height: 170,
+    borderRadius: 5,
+    justifyContent: 'center',
+    color: 'white',
+    elevation: 4,
+  },
+  memoBoxTextStyle: {
+    color: "white",
+    marginLeft: 10,
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular'
+  },
+
+});
+
+export default styles;
