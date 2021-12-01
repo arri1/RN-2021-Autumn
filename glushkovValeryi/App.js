55bce31530e8ef109ee68d13185005966d9e6447
Valery Glushkov <valery.Glushkov@mytona.com>
2021-12-01 19:36:57 +0900

    remove reducer and store to separate files

--- glushkovValeryi/App.js
+++ glushkovValeryi/App.js
@@ -1,30 +1,14 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
 
 import TabNavigator from "./components/routers/TabNavigator";
+import Store from "./components/common/Store";
 
-const defaultState = {
-    points: 0,
-}
-
-const reducer = (state = defaultState, aciton) => {
-    switch(aciton.type) {
-        case "ADD_POINTS":
-            return {...state, points: state.points + aciton.newPoints}
-        case "GET_POINTS":
-            return {...state, points: state.points - aciton.newPoints}
-        default:
-            return state
-    }
-}
-
-const store = createStore(reducer)
 
 const App = () => {
     return(
-        <Provider store={store}>
+        <Provider store={Store}>
             <NavigationContainer>
                 <TabNavigator />
             </NavigationContainer>
