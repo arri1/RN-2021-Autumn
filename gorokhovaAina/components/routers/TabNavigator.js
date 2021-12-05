c02c51afa9dc004c160ec3c6711f96e3405c1858
aino198 <36023401+aino198@users.noreply.github.com>
2021-12-05 20:47:49 +0900

    Подправлены стили как в Figma, добавлены иконки

--- gorokhovaAina/components/routers/TabNavigator.js
+++ gorokhovaAina/components/routers/TabNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import { Image, StyleSheet } from 'react-native';
 import Lab1 from '../screens/lab1';
 import Lab2 from '../screens/lab2';
 
@@ -8,10 +9,45 @@ const Tab = createBottomTabNavigator();
 const TabNavigator = () => {
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Lab1" component={Lab1} />
-            <Tab.Screen name="Lab2" component={Lab2} />
+            <Tab.Screen 
+                name = "Lab1" 
+                component = {Lab1} 
+                options = {{
+                    tabBarIcon: ({focused}) => (
+                        <Image 
+                            source = {require('../../img/icon1.png')}
+                            style = {styles.icon}
+                        />
+                    ),
+                    tabBarLabel: () => {
+                        return null
+                    },
+                }}
+            />
+            <Tab.Screen 
+                name = "Lab2" 
+                component = {Lab2}
+                options = {{
+                    tabBarIcon: ({focused}) => (
+                        <Image 
+                            source = {require('../../img/icon2.png')}
+                            style = {styles.icon}
+                        />
+                    ),
+                    tabBarLabel: () => {
+                        return null
+                    },
+                }}
+            />
         </Tab.Navigator>
     );
 };
+
+const styles = StyleSheet.create({
+    icon:{
+        width: 35,
+        height: 35
+    }
+});
 
 export default TabNavigator;
