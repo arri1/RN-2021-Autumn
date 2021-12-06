95eb9d238ce286a52ceb42ff69c12f95615c3b8d
skryabinNikolay <saykolyan1996@gmail.com>
2021-12-07 02:41:08 +0900

    Fixed style in TabNavigator and minor fixes in Lab 3 (as well as upgraded Figma)

--- skryabinNikolay/components/routers/TabNavigator.js
+++ skryabinNikolay/components/routers/TabNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import {StyleSheet, Text, View, Image} from 'react-native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Lab1 from '../screens/lab1';
 import Lab2 from '../screens/lab2';
@@ -20,61 +20,42 @@ const TabNavigation = () => {
                     elevation: 0,
                     borderRadius: 10,
                     height: 70,
-                    ...styles.shadow
                 }
             }}
         >
-            <Tab.Screen 
-                name = "Lab1" 
-                component = {Lab1} 
+            <Tab.Screen name = "Lab1" component = {Lab1} 
                 options = {{
                     tabBarIcon: ({focused}) => (
                         <View style = {{alignItems: 'center', justifyContent: 'center', top: 3}}>
-                            <Image 
-                                source = {require('../../icons/number_lab1.png')}
+                            <Image source = {require('../../icons/number_lab1.png')}
                                 resizeMode = "contain"
-                                style={{
-                                    width: 60,
-                                    height: 60,
-                                }}
+                                style={{width: 60, height: 60,}}
                             />
                         </View>
                     ),
                 }}
             />
             
-            <Tab.Screen 
-                name = "Lab2" 
-                component = {Lab2} 
+            <Tab.Screen name = "Lab2" component = {Lab2} 
                 options = {{
                     tabBarIcon: ({focused}) => (
                         <View style = {{alignItems: 'center', justifyContent: 'center', top: 3}}>
-                            <Image 
-                                source = {require('../../icons/number_lab2.png')}
+                            <Image source = {require('../../icons/number_lab2.png')}
                                 resizeMode = "contain"
-                                style={{
-                                    width: 60,
-                                    height: 60,
-                                }}
+                                style={{width: 60, height: 60,}}
                             />
                         </View>
                     ),
                 }}
             />
 
-            <Tab.Screen 
-                name = "Lab3" 
-                component = {Lab3} 
+            <Tab.Screen name = "Lab3" component = {Lab3} 
                 options = {{
                     tabBarIcon: ({focused}) => (
                         <View style = {{alignItems: 'center', justifyContent: 'center', top: 3}}>
-                            <Image 
-                                source = {require('../../icons/number_lab3.png')}
+                            <Image source = {require('../../icons/number_lab3.png')}
                                 resizeMode = "contain"
-                                style={{
-                                    width: 60,
-                                    height: 60,
-                                }}
+                                style={{width: 60, height: 60,}}
                             />
                         </View>
                     ),
@@ -84,18 +65,5 @@ const TabNavigation = () => {
         </Tab.Navigator>
     );
 }
-
-const styles = StyleSheet.create({
-    shadow: {
-        shadowColor: 'ffffff',
-        shadowOffset: {
-            width: 10,
-            height: 10,
-        },
-        shadowOpacity: 0.25,
-        shadowRadius: 3.5,
-        elevation: 5
-    }
-});
 
 export default TabNavigation;
