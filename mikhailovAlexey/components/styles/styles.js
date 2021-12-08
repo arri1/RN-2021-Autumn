99b08f29a8313c8daa51fd19235f81bd23ff9915
lex1olnk <lex1olnk@gmail.com>
2021-12-08 18:56:11 +0900

    lab 2

--- mikhailovAlexey/components/styles/styles.js
+++ mikhailovAlexey/components/styles/styles.js
@@ -6,10 +6,50 @@ const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     backgroundColor: '#FFD232',
-    height: 1120
+    height: '100%',
   },
   containerBackgroundColor: {
     backgroundColor: '#FFD232',
+  },
+  buttonRow: {
+    flexBasis: 100,
+    justifyContent: "space-evenly",
+    marginVertical: 20
+  },
+  boxSize: {
+    backgroundColor: '#FF8B03',
+    borderRadius: 5,
+    width: 380,
+    margin: 5,
+    height: 100,
+    justifyContent: 'center',
+    color: 'white',
+    elevation: 4,
+  },
+  boxTextStyle: {
+    color: "white",
+    marginLeft: 10,
+    fontSize: 16,
+    fontFamily: 'Montserrat-Regular'
+  },
+  navBarBox: {
+    top: 0,
+    height: 75,
+    backgroundColor: '#454545',
+    color: 'white',
+  },
+  navBarIcon : {
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: "#FF8B03",
+    width: 50,
+    height: 50,
+    borderRadius: 5,
+  },
+  navBarIconText : {
+    color: 'white',
+    fontSize: 14,
+    fontFamily: 'Montserrat-Regular',
   },
   button: {
     justifyContent: "center",
@@ -18,13 +58,13 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 5,
     backgroundColor: '#FF8B03',
-    elevation: 4
+    elevation: 4,
   },
   buttonText: {
     fontFamily: 'Montserrat-Regular',
     fontSize: 20,
-    color: 'white'
-  }
+    color: 'white',
+  },
 });
 
 export default styles;
