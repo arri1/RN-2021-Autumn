94aa81722ef8d6128cc073c67263138e58b02de1
bonus god <nyxassasin15@gmail.com>
2021-12-08 11:52:57 +0900

    lab2

---
+++ evgeniiSleptsov/components/screens/lab2.js
@@ -0,0 +1,48 @@
+import React, {useState, useEffect} from 'react';
+import {Text, StyleSheet, ScrollView} from 'react-native';
+import axios from 'axios';
+
+const Lab2 = () => {
+  const [data, dataSet] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+        dataSet(response.data);
+      })
+      .catch(() => {});
+  });
+
+  return (
+    <ScrollView style={styles.from}>
+      {data.map(item => (
+        <Text style={styles.item} key={item.id}>
+          {item.title}
+        </Text>
+      ))}
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  item: {
+    marginTop: '5%',
+    marginBottom: '0%',
+    margin: '5%',
+    padding: 15,
+    height: 50,
+    width: '90%',
+    backgroundColor: '#CC99FF',
+    borderRadius: 15,
+    fontSize: 15,
+  },
+  form: {
+    margin: 0,
+    padding: 0,
+    width: '100%',
+    backgroundColor: '#000000',
+  },
+});
+
+export default Lab2;
