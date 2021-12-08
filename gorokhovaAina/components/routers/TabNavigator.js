80759768193f6518248846ab86c985d5b42a6252
aino198 <36023401+aino198@users.noreply.github.com>
2021-12-08 14:53:11 +0900

    lab 3

--- gorokhovaAina/components/routers/TabNavigator.js
+++ gorokhovaAina/components/routers/TabNavigator.js
@@ -3,6 +3,7 @@ import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import { Image, StyleSheet } from 'react-native';
 import Lab1 from '../screens/lab1';
 import Lab2 from '../screens/lab2';
+import Lab3 from '../screens/lab3';
 
 const Tab = createBottomTabNavigator();
 
@@ -31,6 +32,21 @@ const TabNavigator = () => {
                     tabBarIcon: ({focused}) => (
                         <Image 
                             source = {require('../../img/icon2.png')}
+                            style = {styles.icon}
+                        />
+                    ),
+                    tabBarLabel: () => {
+                        return null
+                    },
+                }}
+            />
+            <Tab.Screen 
+                name = "Lab3" 
+                component = {Lab3}
+                options = {{
+                    tabBarIcon: ({focused}) => (
+                        <Image 
+                            source = {require('../../img/icon3.png')}
                             style = {styles.icon}
                         />
                     ),
