6cab6fabff4d6e4dc20b8fa5172b5999c398a499
Silverthg <fmosorkin@gmail.com>
2021-12-28 06:10:27 +0900

    Сделал Лаб4 и сверстал по фигме

--- fedotMosorkin/components/screens/lab1.js
+++ fedotMosorkin/components/screens/lab1.js
@@ -1,29 +1,75 @@
 import React, {useState, useEffect} from 'react';
-import {StyleSheet, Text, View, Button, TouchableOpacity} from 'react-native';
+import {StyleSheet, Text, View, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
-  container: {
+  main: {
     flex: 1,
+  },
+  container: {
     alignItems: 'center',
     justifyContent: 'center',
+    width: 323,
+    height: 258,
+    marginLeft: 44,
+    marginTop: 96,
+    backgroundColor: '#FFFFFF',
+    borderRadius: 30,
   },
   text: {
-    color: '#fff',
+    color: 'black',
     margin: 12,
+    fontFamily: 'rye',
   },
-  button: {
+  textContainer: {
+    color: 'black',
+    margin: 18,
+    fontFamily: 'rye',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingVertical: 10,
-    borderRadius: 4,
-    paddingHorizontal: 30,
-    elevation: 3,
-    backgroundColor: '#F80012',
+  },
+  button: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 121,
+    height: 53,
+    backgroundColor: '#34C759',
+    borderRadius: 30,
+    marginTop: 20,
+  },
+  button1: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 50,
+    borderBottomLeftRadius: 100,
+    borderBottomRightRadius: 0,
+    borderTopLeftRadius: 100,
+    borderTopRightRadius: 0,
+    backgroundColor: '#34C759',
+  },
+  button2: {
+    marginLeft: 21,
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 100,
+    height: 50,
+    borderBottomLeftRadius: 0,
+    borderBottomRightRadius: 100,
+    borderTopLeftRadius: 0,
+    borderTopRightRadius: 100,
+    backgroundColor: '#34C759',
+  },
+  container1: {
+    marginTop: 50,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
 const Lab1 = () => {
-  const [color, setColor] = useState('#009E8E');
+  const [color, setColor] = useState('#7B47E9');
   const [count, setCount] = useState(1);
   const [name, setName] = useState(
     'Не плюй в колодец, пригодится воды напиться.',
@@ -51,28 +97,33 @@ const Lab1 = () => {
     }
   });
   return (
-    <View style={[styles.container, {backgroundColor: color}]}>
+    <View style={[styles.main, {backgroundColor: color}]}>
+      <View style={styles.container}>
+        <Text style={styles.textContainer}>Есть такая пословица: {name}</Text>
+      </View>
+      <View style={styles.container1}>
+        <TouchableOpacity
+          style={styles.button1}
+          onPress={() => {
+            setCount(count - 1);
+          }}>
+          <Text>Назад</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button2}
+          onPress={() => {
+            setCount(count + 1);
+          }}>
+          <Text>След</Text>
+        </TouchableOpacity>
+      </View>
       <TouchableOpacity
         style={styles.button}
         onPress={() =>
           setColor('#' + Math.floor(Math.random() * 21341241).toString(16))
-        }
-      />
-      <Text style={styles.text}>Есть такая пословица: {name}</Text>
-      <Button
-        title="Next"
-        color="#3415B0"
-        onPress={() => {
-          setCount(count + 1);
-        }}
-      />
-      <Button
-        title="Back"
-        color="#3415B0"
-        onPress={() => {
-          setCount(count - 1);
-        }}
-      />
+        }>
+        <Text style={styles.text}>Цвет</Text>
+      </TouchableOpacity>
     </View>
   );
 };
