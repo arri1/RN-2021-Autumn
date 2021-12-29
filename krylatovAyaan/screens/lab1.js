7024d0a58fbee2daabfa5b1bb88d1eee5e45f8d3
kaibob222 <43156001+kaibob222@users.noreply.github.com>
2021-12-29 18:25:07 +0900

    lab1 change button

--- krylatovAyaan/screens/lab1.js
+++ krylatovAyaan/screens/lab1.js
@@ -1,46 +1,62 @@
 import React, {useState} from 'react';
-import {Text, View, Button, StyleSheet} from 'react-native';
+import {Text, View, Button, StyleSheet, TouchableOpacity} from 'react-native';
 
 const Lab1 = () => {
   const [count, setCount] = useState(0);
-  var [color] = useState('#476DD5');
+  var [color, setColor] = useState('#FFBA73');
+
+  const changeColor = () => {
+    if (count === 0) {
+      setColor('#F13C73');
+    }
+    if (count === 1) {
+      setColor('#476DD5');
+    }
+    if (count === 2) {
+      setCount(count - 3);
+      setColor('#FFBA73');
+    }
+  };
+
+  const onPressIvent = () => {
+    changeColor();
+    setCount(count + 1);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>dont touch button {count}</Text>
-      <Button
-        color={changeColor(count, color, setCount)}
-        title="Smash me!"
-        onPress={() => {
-          setCount(count + 1);
-          changeColor(count, color, setCount);
-          color = color;
-        }}
-      />
+      <Text style={styles.text}>The truth is that there is no button</Text>
+      <TouchableOpacity
+        style={[styles.button1, {backgroundColor: color}]}
+        title="Click me!"
+        onPress={onPressIvent}>
+        <Text style={styles.buttontext}>Click me</Text>
+      </TouchableOpacity>
     </View>
   );
 };
-const changeColor = (count, color, setCount) => {
-  if (count === 0) {
-    color = '#FFBA73';
-  }
-  if (count === 1) {
-    color = '#476DD5';
-  }
-  if (count === 2) {
-    color = '#F13C73';
-  }
-  if (count === 3) {
-    setCount(count - 3);
-    color = '#FFBA73';
-  }
-  return color;
-};
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  button1: {
+    width: 100,
+    height: 32,
+    borderRadius: 15,
+  },
+  buttontext: {
+    top: 5,
+    textAlign: 'center',
+  },
+  text: {
+    fontFamily: 'Roboto',
+    fontSize: 12,
+    left: 110,
+    position: 'absolute',
+    top: 230,
   },
 });
 export default Lab1;
