4eb1b129fe2379a157dba09fc816703bce233673
ruaziat <kunney2797@gmail.com>
2021-12-31 00:17:01 +0900

    lab4

--- kunneyKoryakina/components/screens/Lab2.js
+++ kunneyKoryakina/components/screens/Lab2.js
@@ -1,42 +1,75 @@
 import React, {useState, useEffect} from 'react';
-import {Text, StyleSheet, ScrollView} from 'react-native';
-import axios from 'axios';
+import {View, Text, ActivityIndicator, StyleSheet, ScrollView, SafeAreaView, TouchableOpacity,} from 'react-native';
+import {useDispatch, useSelector} from 'react-redux';
+import {checkItem} from '../../store/tasks';
 
 const Lab2 = () => {
-  const [data, dataSet] = useState([]);
+  const data = useSelector(state => state.data.value);
+  const dispatch = useDispatch();
 
-  useEffect(() => {
-    axios
-      .get('https://jsonplaceholder.typicode.com/todos')
-      .then(response => {
-        dataSet(response.data);
-      })
-      .catch(() => {});
-  });
+  const content = () => {
+    return (
+      <ScrollView>
+        {data.map(item => {
+            return (
+              <TouchableOpacity
+              key={item.id}
+              style={[
+                styles.item,
+                item.checked ? {backgroundColor: '#436D9F'} : undefined,
+              ]}
+              onPress={() => {
+                dispatch(checkItem(item.id));
+              }}>
+              <Text style={styles.title}>{item.title}</Text>
+              <Text style={styles.text}>{item.body}</Text>
+              <Text style={styles.email}>{item.email}</Text>
+            </TouchableOpacity>
+          );
+        })}
+      </ScrollView>
+    );
+  };
 
   return (
-    <ScrollView>
-      {data.map(item => (
-        <Text style={styles.item} key={item.id}>
-          {item.title}
-        </Text>
-      ))}
-    </ScrollView>
+    <SafeAreaView style={styles.main}>
+      <View style={styles.container}>
+        {data ? content() : <ActivityIndicator color={'red'} />}
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  main: {
+    height: '100%',
+    backgroundColor: '#FFFFFF',
+  },
   item: {
-    marginTop: 15,
-    marginBottom: 10,
-    margin: 20,
-    width: 350,
-    padding: 15,
-    height: 50,
-    backgroundColor: '#E6E6FA',
-    fontSize: 15,
-    borderWidth: 2,
+    padding: 0,
+    borderRadius: 0,
+    marginBottom: 20,
     borderColor: '#444586',
+    borderWidth: 2,
+    backgroundColor: '#E6E6FA',
+  },
+  container: {
+    flex: 1,
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  text: {
+    marginTop: 0,
+    marginLeft: 10,
+  },
+  email: {
+    marginTop: 20,
+    textAlign: 'right',
   },
 });
 
