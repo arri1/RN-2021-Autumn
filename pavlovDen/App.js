8f15d62b5bdefd2c284ec9d37c1cfb4f561e574d
zereckt <43085576+zereckt@users.noreply.github.com>
2021-12-31 00:11:40 +0900

    Add files via upload

--- pavlovDen/App.js
+++ pavlovDen/App.js
@@ -3,17 +3,13 @@ import {NavigationContainer} from '@react-navigation/native';
 import TopTabNavigator from './components/screens/Lab5TabNavigator';
 import store from './store';
 import {Provider} from 'react-redux';
-import {ApolloProvider} from '@apollo/client';
-import client from './components/apollo/apollo';
 
 const App = () => {
   return (
     <Provider store={store}>
-      <ApolloProvider client={client}>
         <NavigationContainer>
           <TopTabNavigator />
         </NavigationContainer>
-      </ApolloProvider>
     </Provider>
   );
 };
