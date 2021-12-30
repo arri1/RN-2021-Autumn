9f1117ce5736d31da2157df3c9bbd17594892cf1
zereckt <43085576+zereckt@users.noreply.github.com>
2021-12-31 00:14:52 +0900

    Lab4

--- pavlovDen/App.js
+++ pavlovDen/App.js
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import TopTabNavigator from './components/screens/Lab5TabNavigator';
 import store from './store';
 import {Provider} from 'react-redux';
 
