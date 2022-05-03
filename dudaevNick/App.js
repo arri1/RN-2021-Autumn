daf37cfafed1a92664db1b985e7a4883caaae062
dudaev24 <70939177+dudaev24@users.noreply.github.com>
2022-05-03 21:27:23 +0900

    Lab2

--- dudaevNick/App.js
+++ dudaevNick/App.js
@@ -1,13 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import { Font } from 'expo';
+import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import TabNavigator from './components/navigations/TabNavigator';
+import store from './store';
+import {Provider} from 'react-redux';
+
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <TabNavigator></TabNavigator>
-    </NavigationContainer>
+    <Provider store={store}>
+      <NavigationContainer>
+        <TabNavigator />
+      </NavigationContainer>
+    </Provider>
   );
 };
 
