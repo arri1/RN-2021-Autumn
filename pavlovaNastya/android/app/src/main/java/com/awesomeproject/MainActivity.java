9bc8c7ebf721d2f422aa659082f341a24c3b6f30
vacuuminhead <pav.anast1995@gmail.com>
2022-06-09 15:11:03 +0900

    lab2

--- pavlovaNastya/android/app/src/main/java/com/awesomeproject/MainActivity.java
+++ pavlovaNastya/android/app/src/main/java/com/awesomeproject/MainActivity.java
@@ -1,10 +1,16 @@
 package com.awesomeproject;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "pavlovaNastya";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 }
 
