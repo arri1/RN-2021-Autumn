31b5d4c3e31047e535d26269bf22ea940af3fcca
vacuuminhead <pav.anast1995@gmail.com>
2022-06-10 21:30:03 +0900

    lab4

--- pavlovaNastya/App.js
+++ pavlovaNastya/App.js
@@ -1,12 +1,16 @@
 import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import BottomTab from './components/navigator/tabs';
+import store from './store';
+import {Provider} from 'react-redux';
 
 const App = () =>{
  return (
-   <NavigationContainer>
-     <BottomTab/>
-   </NavigationContainer>
+   <Provider store={store}>
+     <NavigationContainer>
+       <BottomTab/>
+     </NavigationContainer>
+   </Provider>
  );
 };
 export default App;
